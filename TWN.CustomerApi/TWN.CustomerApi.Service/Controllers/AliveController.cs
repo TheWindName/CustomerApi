212cf7a9fd666dc1483b1e6c25b4858623e19db1
Iván Rodríguez <ivanrodriguezmillan@outlook.com>
2022-03-20 01:29:52 +0100

    Se ha creado el primer controlador que nos sirve para saber si la aplicación, una vez desplegada, está corriendo.

---
+++ TWN.CustomerApi/TWN.CustomerApi.Service/Controllers/AliveController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TWN.CustomerApi.Service.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ApiVersionNeutral]
+    [ApiController]
+    [Route("/")]
+    [Route("alive")]
+    public class AliveController : ControllerBase
+    {
+        /// <summary>
+        /// Get Method which returns api status code 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public OkResult Get()
+        {
+            return base.Ok();
+        }
+    }
+}
